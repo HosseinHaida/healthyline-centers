a552ecda7e1be76963760fb01f98f7bc09b68bc0
Hossein Heidari <squanderness@gmail.com>
2024-01-09 19:29:21 +0330

    update org status

--- api/routes/manage.js
+++ api/routes/manage.js
@@ -4,12 +4,16 @@ const verifyAuth = require("../src/middlewares/verifyAuth.js")
 const verifyAdmin = require("../src/middlewares/verifyAdmin.js")
 
 const { fetchCenters } = require("../src/components/manage/manageCenters.js")
-const { fetchOrgs } = require("../src/components/manage/manageOrgs.js")
+const {
+  fetchOrgs,
+  updateOrgStatus,
+} = require("../src/components/manage/manageOrgs.js")
 
 const router = express.Router()
 
 // GET
 router.get("/centers/list", verifyAuth, verifyAdmin, fetchCenters)
 router.get("/orgs/list/:org_status?", verifyAuth, verifyAdmin, fetchOrgs)
+router.post("/orgs/status/update", verifyAuth, verifyAdmin, updateOrgStatus)
 
 module.exports = router
--- api/src/components/manage/manageOrgs.js
+++ api/src/components/manage/manageOrgs.js
@@ -8,8 +8,17 @@ const { errMessages } = require("../../helpers/errMessages")
 const fetchOrgs = async (req, res) => {
   const { org_status } = req.params
   try {
-    const list = await db("orgs")
-      .select("*")
+    const query = db("orgs")
+      .select(
+        "orgs.*",
+        "orgs_checklist_approval.status",
+        "orgs_checklist_approval.has_contracts_recently",
+        "orgs_checklist_approval.is_center_unique",
+        "orgs_checklist_approval.is_code_sync",
+        "orgs_checklist_approval.is_login_info_correct",
+        "orgs_checklist_approval.admin_comment",
+        "centers.registration_name"
+      )
       .join(
         "orgs_checklist_approval",
         "orgs.id",
@@ -18,6 +27,9 @@ const fetchOrgs = async (req, res) => {
       )
       .join("centers", "orgs.center_id", "=", "centers.id")
 
+    if (org_status) query.where({ status: org_status })
+
+    const list = await query
     successMessage.list = list
     res.status(status.success).send(successMessage)
   } catch (error) {
@@ -25,4 +37,33 @@ const fetchOrgs = async (req, res) => {
   }
 }
 
-module.exports = { fetchOrgs }
+const updateOrgStatus = async (req, res) => {
+  const { user_id, username } = req.user
+  const {
+    org_id,
+    hasContractsRecently: has_contracts_recently,
+    isCenterUnique: is_center_unique,
+    isCodeSync: is_code_sync,
+    isLoginInfoCorrect: is_login_info_correct,
+    adminComment: admin_comment,
+    status: org_status,
+  } = req.body
+
+  try {
+    await db("orgs_checklist_approval")
+      .update({
+        has_contracts_recently,
+        is_center_unique,
+        is_code_sync,
+        is_login_info_correct,
+        admin_comment,
+        status: org_status,
+      })
+      .where({ org_id })
+    res.status(status.success).send()
+  } catch (error) {
+    return catchError(errMessages.operationFailed, "error", res, error)
+  }
+}
+
+module.exports = { fetchOrgs, updateOrgStatus }
